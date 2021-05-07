957b5f60805b3a7814dca2fa6374236dbb7b682d
Alastair Callum <callum.alastair@gmail.com>
2021-05-07 08:33:31 +0100

    Update config.py
    - Changed default emulation status to False

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # this project comes with CV2, a basic display emulation package for SPI displays
 # Set this to False to run on your Pi, or True to run on your windows desktop. note, it's small!
-RUN_EMULATOR = True
+RUN_EMULATOR = False
 
 # If True, the shown will be interpolated between the latest and previous values. Simply giving the display more 'movement'
 # If True, it will take a couple cycles of the various screens for the interpolated values to catch up with latest
